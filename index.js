118acba3ce811ed1ada6573f0e0b7a96cadc15a8
Zayn Baloch <zainurrehman005@gmail.com>
2024-05-24 09:46:23 +0500

    changed with new versions

--- index.js
+++ index.js
@@ -1,20 +1,34 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
-// 1) Computer will generate a random number - Done
-// 2) User input for guessing number - Done
-// 3) Compare user input with computer genrated number and show result - Done
-const randomNumber = Math.floor(Math.random() * 6 + 1);
-const answers = await inquirer.prompt([
-    {
-        name: "userGuessedNumvber",
-        type: "number",
-        message: "Please guess a number between 1 to 6: ",
-    },
-]);
-if (answers.userGuessedNumvber === randomNumber) {
-    console.log("Congratulation! You guessed the right number.");
+const maxGuesses = 3; // Maximum number of guesses allowed
+async function guessNumber() {
+    let guessesleft = maxGuesses;
+    let correctGuess = false; // Flag to track if the user guessed the number correctly.
+    while (guessesleft > 0 && !correctGuess) {
+        const randomNumber = Math.floor(Math.random() * 10 + 1);
+        const answers = await inquirer.prompt([
+            {
+                type: "number",
+                name: "userGuess",
+                message: `Guess a number between 0 to 10 and write it (You have ${guessesleft} guesses left);`,
+            },
+        ]);
+        const { userGuess } = answers;
+        console.log(`Guessed No: ${userGuess}, Correct answer is: ${randomNumber}`);
+        if (userGuess === randomNumber) {
+            console.log("Correct Answer! Congratulation, You won!");
+            correctGuess = true; // Set the flag to true if the guessed number is correct.
+        }
+        else {
+            console.log("Wrong Answer!");
+            guessesleft--; // Decrement the number of guesses left
+            if (guessesleft > 0) {
+                console.log(`You have ${guessesleft} guesses left.`);
+            }
+        }
+    }
+    if (!correctGuess) {
+        console.log(`Out of guesses. Better luck next time.!`);
+    }
 }
-else {
-    console.log("Aww! You guessed the wrong number.");
-}
-console.log(answers);
+guessNumber();
